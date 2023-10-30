618e1570fe72d6d331bb2e815adaf97ea2911229
Liebe974 <137163247+Liebe974@users.noreply.github.com>
2023-10-30 12:02:06 +0400

    maj

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let preScrollbar = window.scrollY;
 window.addEventListener("scroll", () => {
    const scrollbar = window.scrollY;
     if(scrollbar > preScrollbar) {
-        document.getElementById("navbar").style.display = "none";
+        document.querySelector("#navbar").style.display = "none";
     } else {
-        document.getElementById("navbar").style.display = "grid";
+        document.querySelector("#navbar").style.display = "grid";
     }
     ;});
