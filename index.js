368711969e8d3ad1c5bf795f67b9a74ef57ebc5d
Liebe974 <137163247+Liebe974@users.noreply.github.com>
2023-10-30 13:38:46 +0400

    js ok

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ let preScrollbar = window.scrollY;
 window.addEventListener("scroll", () => {
    const scrollbar = window.scrollY;
     if(scrollbar > preScrollbar) {
-        document.querySelector("#navbar").style.display = "none";
+        document.querySelector("#navbar").style.top = "-60px";
     } else {
-        document.querySelector("#navbar").style.display = "grid";
+        document.querySelector("#navbar").style.top = "0";
     }
     ;});
+    window.addEventListener("scroll", function (e) {
+        console.log(window.scrollY);
+      });
+      
+      // Event listener "scroll" sur window
+      
+      // Récupérer valeur du scroll en Y : window.scrollY
+      
+      // Savoir quand on monte ou descend 
+      
+      // Si on monte, Faire apparaitre la navbar : navbar.style.top = 0px;
+      
+      // Si on descend, Faire disparaitre la navbar : navbar.style.top = -60px;
